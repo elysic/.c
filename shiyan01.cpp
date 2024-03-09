66a7300d6f0f1d36b9ccc3dcf7e624030362a7b9
elysic <158454668+elysic@users.noreply.github.com>
2024-03-09 20:51:41 +0800

    Add files via upload

---
+++ shiyan01.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/*void main()
+{
+	printf("********安232刘文杰239044052********\n");
+	printf("********Good Morning*****\n");
+	printf("***************************\n");
+}*/
+#include<stdio.h>
+void main()
+{
+	int a=0,b=20;
+	unsigned int c=0xff;
+	long D;
+	D=301;
+	printf("a=%d\n",a);
+	printf("b=%d\n",b);
+	printf("c=%d\n",c);
+	printf("D=%d\n",D);
+	printf("安232刘文杰239044052\n");
+}
